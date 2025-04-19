5dd536438deed696d33f891737d3a3e88cec5039
Amzt-pixel <abishmaraniya@gmail.com>
2025-04-20 00:05:07 +0530

    Update dictionary2.js

--- dictionary2.js
+++ dictionary2.js
@@ -245,7 +245,7 @@ function shuffleArray(array) {
   }
   return shuffled;
 }
-
+/*
 function initSearch() {
   const searchInput = document.getElementById("wordSearch");
   const searchButton = document.querySelector(".searchBar-button");
@@ -279,6 +279,8 @@ function initSearch() {
     }
   });
 }
+*/
+
 function handleSearch(e) {
   // Only proceed if in dynamic mode OR this is a manual trigger (added line)
   if (!isSearchActive && e.type !== 'manual') return;
