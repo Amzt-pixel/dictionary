8850f0c39e63a5dbb50a9bf871c26683eaaaa616
Amzt-pixel <abishmaraniya@gmail.com>
2025-05-18 14:06:12 +0530

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -1075,9 +1075,6 @@ function viewRootWords() {
   <div class="root-word-result" data-word="${item.word}">
     <span class="result-number">${index + 1}.</span>
     <span class="result-word">${item.word}</span>
-    /*
-    <span class="root-id">– ${item.numId}</span>
-    */
   </div>
 `).join('');
     document.querySelectorAll('.root-word-result').forEach(item => {
