db559c60a9f71108f790b8c837e0da26cedafb20
Amzt-pixel <abishmaraniya@gmail.com>
2025-05-21 08:46:31 +0530

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -94,7 +94,11 @@ document.getElementById("infoButton").addEventListener("click", () => {
   document.getElementById("metaOverlay").style.display = "block";
   showMetadata();
 });
-
+document.getElementById("infoButtonAlt").addEventListener("click", () => {
+  document.getElementById("infoPopup").style.display = "block";
+  document.getElementById("metaOverlay").style.display = "block";
+  showMetadata();
+});
 document.getElementById("closeInfo").addEventListener("click", () => {
   document.getElementById("infoPopup").style.display = "none";
   document.getElementById("metaOverlay").style.display = "none";
